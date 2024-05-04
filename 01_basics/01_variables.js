8662104470c064c31cc7b71633ee54a202e50089
Akash Raj <105816479+akashraj1238@users.noreply.github.com>
2024-05-04 14:15:39 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const rollNumber = 36
+var name = "Akash"
+let emailId = "akashraj@gmail.com"
+address = "India"
+let university;
+
+// rollNumber = 23
+console.log(rollNumber)
+
+console.table([rollNumber,name,emailId,address,university])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
